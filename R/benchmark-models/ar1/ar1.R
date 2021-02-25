415b68657421d5f6e39e245568bba284adf4011c
mwelz <welz@ese.eur.nl>
2021-02-25 19:33:33 +0100

    Updated AR(1) models to have a consistent dependent variable with the other models

--- R/benchmark-models/ar1/ar1.R
+++ R/benchmark-models/ar1/ar1.R
@@ -3,9 +3,12 @@
 #' Estimation is done by OLS.
 #' 
 #' Author: mwelz
-#' Last changed: Feb. 19, 2021.
+#' Last changed: Feb. 25, 2021.
 #' ------------------------------------------------------------------------------
 rm(list = ls()) ; cat("\014")
+
+# load helper functions
+source(paste0(getwd(), "/R/cointegration/cointegration-finder/helper-functions.R"))
 
 # load & prepare data, being GDP index with 1960 = 100 ----
 data           <- read.csv(paste0(getwd(), "/data/processed/africa_gdp_index.csv"))
@@ -23,38 +26,40 @@ end.years <- c(2010, 2011, 2012, 2013, 2014, 2015)
 ar1.results <- list()
 
 for(i in 1:length(countries)){
-  series <- stats::ts(data.log[,i], start = c(1960,1), end = c(2016,1), frequency = 1)
   
+  # variable of interest
+  y.d.lag1 <- data.log[,i] - lag.p(data.log[,i], 1)
+  names(y.d.lag1) <- rownames(data.log)
+
   # initialize
-  out <- matrix(NA_real_, length(end.years), 5)
+  out <- matrix(NA_real_, length(end.years), 4)
   rownames(out) <- paste0("(1960, ", end.years, ")")
   colnames(out) <- c("AR1.intercept", "AR1.coefficient", "one.step.ahead.fcast",
-                     "true.value", "squared.fcast.error")
+                     "true.value")
 
   for(j in 1:length(end.years)){
-    series.subset <- stats::window(series, end = c(end.years[j],1))
-    
-    # fit AR(1) model with intercept by OLS
-    ar.mod        <- ar.ols(series.subset, order.max = 1, intercept = TRUE)
+    t               <- end.years[j]
+    y.d.lag1.subset <- y.d.lag1[1:which(names(y.d.lag1) == t)]
+
+    # fit AR(1) model with intercept by OLS (first observation is NA due to lagging, so drop it)
+    ar.mod        <- ar.ols(y.d.lag1.subset[-1], order.max = 1, intercept = TRUE,
+                            demean = FALSE, aic = FALSE)
     ar.intercept  <- ar.mod$x.intercept
     ar.coef       <- as.numeric(ar.mod$ar)
     
     # perform 1-step ahead forecast
     ar.1stepfcast <- ar.intercept +
-      ar.coef * series.subset[length(series.subset)]
+      ar.coef * y.d.lag1.subset[which(names(y.d.lag1.subset) == t)]
     
     # evaluate the forecast
-    true.value <- as.numeric(stats::window(series, 
-                                           start = c(end.years[j] + 1, 1), 
-                                           end   = c(end.years[j] + 1, 1)))
+    true.value <- y.d.lag1[which(names(y.d.lag1) == t + 1)]
     
     # store results
-    out[j, "AR1.intercept"]     <- ar.intercept
-    out[j, "AR1.coefficient"]   <- ar.coef
-    out[j, "one.step.ahead.fcast"]  <- ar.1stepfcast
-    out[j, "true.value"]          <- true.value
-    out[j, "squared.fcast.error"] <- (ar.1stepfcast - true.value)^2
-    
+    out[j, "AR1.intercept"]        <- ar.intercept
+    out[j, "AR1.coefficient"]      <- ar.coef
+    out[j, "one.step.ahead.fcast"] <- as.numeric(ar.1stepfcast)
+    out[j, "true.value"]           <- as.numeric(true.value)
+
   } # END end.years
   
   ar1.results[[i]] <- out
