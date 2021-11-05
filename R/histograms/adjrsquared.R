b3e61422fc5686ad7eac536d3a3674cd79ece7a3
mwelz <welz@ese.eur.nl>
2021-11-05 14:54:34 +0100

    Added benchmark histograms

--- R/histograms/adjrsquared.R
+++ R/histograms/adjrsquared.R
@@ -7,6 +7,8 @@ load(paste0(getwd(), "/R/benchmark-models/ols/ols-adjr2.Rdata"))
 # the countries
 countries <- colnames(ols.adjr2)
 
+### 1. SEFM ----
+
 # we only consider the period 1960 to 2015
 ols.adjr2 <- ols.adjr2["smpl1960to2015",]
 
@@ -36,12 +38,66 @@ pdf(file = paste0(getwd(), "/R/histograms/plots/SEFM_adjustedRsquared.pdf"))
 gg <- ggplot(data.frame(adjr2 = adjr2), aes(x = adjr2)) + 
   geom_histogram(color = "black", fill = "gray", bins = 10) +
   theme_bw() +
-  theme(axis.title.x = element_blank(),
+  theme(plot.title = element_text(size = 16),
+        axis.title.x = element_blank(),
         axis.title.y = element_blank(),
-        axis.text = element_text(size = 14))
+        axis.text = element_text(size = 14)) +
+  ggtitle("Adjusted R2 of SEFM")
 print(gg)
 dev.off()
 
 # some summary statistics
 sort(adjr2) # max RWANDA (0.7095), min SOMALIA (0.0385)
 mean(adjr2) # 0.353
+
+
+### 2. benchmark methods ----
+
+load(paste0(getwd(), "/R/benchmark-models/ar1/ar1-results.Rdata"))
+load(paste0(getwd(), "/R/benchmark-models/ima/ima-results.Rdata"))
+load(paste0(getwd(), "/R/benchmark-models/pcr/pcr-results.Rdata"))
+timeframe <- "(1960, 2015)"
+
+# AR(1)
+adjr2 <- sapply(countries, function(i) ar1.results[[i]][timeframe, "adjR2"])
+
+pdf(file = paste0(getwd(), "/R/histograms/plots/AR1_adjustedRsquared.pdf"))
+gg <- ggplot(data.frame(adjr2 = adjr2), aes(x = adjr2)) + 
+  geom_histogram(color = "black", fill = "gray", bins = 10) +
+  theme(plot.title = element_text(size = 16),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text = element_text(size = 14)) +
+  ggtitle("Adjusted R2 of AR(1)")
+print(gg)
+dev.off()
+
+# IMA(1,1)
+adjr2 <- sapply(countries, function(i) ma1.results[[i]][timeframe, "adjR2"])
+
+pdf(file = paste0(getwd(), "/R/histograms/plots/MA1_adjustedRsquared.pdf"))
+gg <- ggplot(data.frame(adjr2 = adjr2), aes(x = adjr2)) + 
+  geom_histogram(color = "black", fill = "gray", bins = 10) +
+  theme_bw() +
+  theme(plot.title = element_text(size = 16),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text = element_text(size = 14)) +
+  ggtitle("Adjusted R2 of IMA(1,1)")
+print(gg)
+dev.off()
+
+# PCR
+adjr2 <- sapply(countries, function(i) pcr.results[[i]][timeframe, "adjR2"])
+
+pdf(file = paste0(getwd(), "/R/histograms/plots/PCR_adjustedRsquared.pdf"))
+gg <- ggplot(data.frame(adjr2 = adjr2), aes(x = adjr2)) + 
+  geom_histogram(color = "black", fill = "gray", bins = 10) +
+  theme_bw() +
+  theme(plot.title = element_text(size = 16),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text = element_text(size = 14)) +
+  ggtitle("Adjusted R2 of PCR")
+print(gg)
+dev.off()
--- R/histograms/mae_and_me.R
+++ R/histograms/mae_and_me.R
@@ -4,6 +4,8 @@ rm(list = ls()) ; cat("\014")
 # load data on MAE and ME ("perf.eval.out")
 load(paste0(getwd(), "/R/summary/performances.Rdata"))
 
+#### 1. SEFM ----
+
 # MAE (mean absolute error)
 mae <- perf.eval.out$SEFM_MAE
 names(mae) <- rownames(perf.eval.out)
@@ -39,9 +41,89 @@ pdf(file = paste0(getwd(), "/R/histograms/plots/SEFM_ME.pdf"))
 gg <- ggplot(data.frame(me = me), aes(x = me)) + 
   geom_histogram(color = "black", fill = "gray", bins = 12) +
   theme_bw() +
-  theme(axis.title.x = element_blank(),
+  theme(plot.title = element_text(size = 16),
+        axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         axis.text = element_text(size = 14)) +
+  ggtitle("ME of SEFM") + 
   xlim(-10, 10)
 print(gg)
 dev.off()
+
+
+
+# MAE (mean absolute error)
+mae <- perf.eval.out$SEFM_MAE
+names(mae) <- rownames(perf.eval.out)
+sort(mae) # min: 0.5539 (Comoros), max: 43.634 (Libya)
+mean(mae) # 4.391
+
+mae.no.libya <- mae[-which(names(mae) == "LIBYA")]
+mean(mae.no.libya) # 3.624
+
+# make a histogram
+library(ggplot2)
+
+pdf(file = paste0(getwd(), "/R/histograms/plots/SEFM_MAE.pdf"))
+gg <- ggplot(data.frame(mae.no.libya = mae.no.libya), aes(x = mae.no.libya)) + 
+  geom_histogram(color = "black", fill = "gray", bins = 15) +
+  theme_bw() +
+  theme(plot.title = element_text(size = 16),
+        axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text = element_text(size = 14)) +
+  ggtitle("MAE of SEFM")
+print(gg)
+dev.off()
+
+
+### 2. benchmark models ----
+methods <- c("AR1", "MA1", "PCR", "RW")
+
+for(method in methods){
+  
+  if(method == "AR1"){
+    nam <- "AR(1)"
+  } else if(method == "MA"){
+    nam <- "IMA(1,1)"
+  } else{
+    nam <- method
+  } 
+  
+  
+  # ME (mean error)
+  me <- perf.eval.out[, paste0(method, "_ME")]
+  
+  # make a histogram
+  pdf(file = paste0(getwd(), "/R/histograms/plots/", method, "_ME.pdf"))
+  gg <- ggplot(data.frame(me = me), aes(x = me)) + 
+    geom_histogram(color = "black", fill = "gray", bins = 12) +
+    theme_bw() +
+    theme(plot.title = element_text(size = 16),
+          axis.title.x = element_blank(),
+          axis.title.y = element_blank(),
+          axis.text = element_text(size = 14)) +
+    ggtitle(paste0(" ME of ", nam))
+  print(gg)
+  dev.off()
+  
+  
+  # MAE 
+  mae <- perf.eval.out[, paste0(method, "_ME")]
+  mae.no.libya <- mae[-which(names(mae) == "LIBYA")]
+
+  # make a histogram
+  pdf(file = paste0(getwd(), "/R/histograms/plots/", method, "_MAE.pdf"))
+  gg <- ggplot(data.frame(mae.no.libya = mae.no.libya), aes(x = mae.no.libya)) + 
+    geom_histogram(color = "black", fill = "gray", bins = 12) +
+    theme_bw() +
+    theme(plot.title = element_text(size = 16),
+          axis.title.x = element_blank(),
+          axis.title.y = element_blank(),
+          axis.text = element_text(size = 14)) +
+    ggtitle(paste0(" MAE of ", nam))
+  print(gg)
+  dev.off()
+  
+}
+
